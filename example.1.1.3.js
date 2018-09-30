0376bbfb509849377dfd385e8d093e66645d0177
kimdahye <dahye4321@naver.com>
2018-09-30 22:58:09 +0900

    20180930 functional programming chapter1, study

--- example.1.1.3.js
+++ example.1.1.3.js
@@ -8,7 +8,6 @@ var users = [
     { id: 7, name: "HI", age: 24 }
 ];
 
-//filter 함수 추가하여 리펙토링
 function filter(list, predicate) {
     var new_list = [];
     for (var i = 0, len = list.length; i < len; i++) {
@@ -17,32 +16,25 @@ function filter(list, predicate) {
     return new_list;
 }
 
-var users = [
-    { id: 1, name: "ID", age: 32 },
-    { id: 2, name: "HA", age: 25 },
-    { id: 3, name: "BJ", age: 32 },
-    { id: 4, name: "PJ", age: 28 },
-    { id: 5, name: "JE", age: 27 },
-    { id: 6, name: "JM", age: 32 },
-    { id: 7, name: "HI", age: 24 }
-];
+function map(list, iteratee) {
+    var new_list = [];
+    for(var i = 0, len = list.length; i < len; i++) {
+        new_list.push(iteratee(list[i]));
+    }
+    return new_list;
+}
 
-//개선
 var temp_users = filter(users, function(user){return user.age < 30;});
 console.log(temp_users.length); // 4
 
-var ages = [];
-for (var i = 0, len = temp_users.length; i < len; i++) {
-    ages.push(temp_users[i].age);
-}
+//개선
+var ages = map(temp_users, function(user) {return user.age;});
 console.log(ages); //[25,28,27,24]
 
-//개선
 var temp_users = filter(users, function(user){return user.age >= 30;});
 console.log(temp_users.length); // 3
 
-var names = [];
-for (var i = 0, len = temp_users.length; i < len; i++) {
-    names.push(temp_users[i].name);
-}
+//개선
+var names = map(temp_users, function(user) {return user.name;});
 console.log(names); //["ID", "BJ", "JM"]
+
---
+++ example.1.2.2.js
@@ -0,0 +1,48 @@
+var users = [
+    { id: 1, name: "ID", age: 32 },
+    { id: 2, name: "HA", age: 25 },
+    { id: 3, name: "BJ", age: 32 },
+    { id: 4, name: "PJ", age: 28 },
+    { id: 5, name: "JE", age: 27 },
+    { id: 6, name: "JM", age: 32 },
+    { id: 7, name: "HI", age: 24 }
+];
+
+function filter(list, predicate) {
+    var new_list = [];
+    for (var i = 0, len = list.length; i < len; i++) {
+        if (predicate(list[i])) new_list.push(list[i]);
+    }
+    return new_list;
+}
+
+function map(list, iteratee) {
+    var new_list = [];
+    for(var i = 0, len = list.length; i < len; i++) {
+        new_list.push(iteratee(list[i]));
+    }
+    return new_list;
+}
+
+//log_length 함수 추가
+function log_length(value) {
+    console.log(value.length);
+    return value;
+}
+
+function bvalue(obj) {
+    return function(key) {
+        return obj[key];
+    }
+}
+
+console.log(log_length(map(
+    filter(users, function(user){return user.age < 30;}), 
+    bvalue('age')
+)));
+
+console.log(log_length(map(
+    filter(users, function(user){return user.age >= 30;}), 
+    bvalue('name')
+)));
+
